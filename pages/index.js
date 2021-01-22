80431e5cab56b0b4201b6cb393652a0f1e547431
Paul Ccari <paulclindo@gmail.com>
2021-01-21 20:40:11 -0500

    add stripe to checkout each products

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import products from "../products.json";
+import { initiateCheckout } from "../lib/payments";
 
 export default function Home() {
+  console.log("NEXT", process.env.NEXT_PUBLIC_STRIPE_API_KEY);
   return (
     <div className={styles.container}>
       <Head>
@@ -26,6 +28,23 @@ export default function Home() {
                 <p>${price}</p>
                 <p>{description}</p>
               </a>
+              <p>
+                <button
+                  className={styles.button}
+                  onClick={() =>
+                    initiateCheckout({
+                      lineItems: [
+                        {
+                          price: id,
+                          quantity: 1,
+                        },
+                      ],
+                    })
+                  }
+                >
+                  Buy Now
+                </button>
+              </p>
             </li>
           ))}
         </ul>
